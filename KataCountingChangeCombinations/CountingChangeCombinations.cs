415ce3ee1b974cd4c97bee7004515709280822d6
king.ho99 <king.ho@titansoft.com.sg>
2018-04-21 14:48:23 +0800

    first test case pass

---
+++ KataCountingChangeCombinations/CountingChangeCombinations.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataCountingChangeCombinations
+{
+    public class CountingChangeCombinations
+    {
+        public static int CountCombinations(int money, int[] coins)
+        {
+            int changeTimes = 0;
+            var orderedCoins = coins.OrderByDescending(x => x).ToList();
+            var remainMoney = money - orderedCoins[0];
+            if (remainMoney - orderedCoins[1] == 0)
+            {
+                changeTimes++;
+            }
+
+            return changeTimes;
+        }
+    }
+}
---
+++ KataCountingChangeCombinationsTests/CountingChangeCombinationsTests.cs
@@ -0,0 +1,30 @@
+﻿using KataCountingChangeCombinations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace KataCountingChangeCombinations.Tests
+{
+    [TestFixture]
+    public class CountingChangeCombinationsTests
+    {
+        [Test()]
+        public void CountCombinationsTest_NoChange_Should_Return_0()
+        {
+            //arrange
+            var money = 11;
+            var coins = new [] {5, 7};
+            var expected = 0;
+
+            //act
+            var acturl = CountingChangeCombinations.CountCombinations(money, coins);
+
+            //assert
+            Assert.AreEqual(expected, acturl);
+        }
+    }
+}
