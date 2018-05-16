82e2eabca41f9d0aa103fae40b7f39437c212ac2
king.ho99 <king.ho@titansoft.com.sg>
2018-05-17 00:01:45 +0800

    add more test case

--- KataFindMissingTerm/KataFindMissingTermTests/FindMissingTermTests.cs
+++ KataFindMissingTerm/KataFindMissingTermTests/FindMissingTermTests.cs
@@ -14,7 +14,7 @@ namespace KataFindMissingTerm.Tests
         [Test()]
         public void FindMissingTest_input_1_3_5_9_7_should_return_7()
         {
-            var list = new List<int>{1,3,5,9};
+            var list = new List<int>{ 1, 3, 5, 9};
             var target = new FindMissingTerm();
             var expected = 7;
 
@@ -23,5 +23,40 @@ namespace KataFindMissingTerm.Tests
             Assert.AreEqual(expected, actual);
         }
 
+        [Test()]
+        public void FindMissingTest_missing_term_is_second_item()
+        {
+            var list = new List<int> { 1, 5, 7, 9, 11 };
+            var target = new FindMissingTerm();
+            var expected = 3;
+
+            var actual = target.FindMissing(list);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test()]
+        public void FindMissingTest_missing_term_is_last_item()
+        {
+            var list = new List<int> { 1, 3, 5, 7, 9 };
+            var target = new FindMissingTerm();
+            var expected = 11;
+
+            var actual = target.FindMissing(list);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test()]
+        public void FindMissingTest_no_missing_term_should_return_last_item()
+        {
+            var list = new List<int> { 3, 5, 7, 9 };
+            var target = new FindMissingTerm();
+            var expected = 11;
+
+            var actual = target.FindMissing(list);
+
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
