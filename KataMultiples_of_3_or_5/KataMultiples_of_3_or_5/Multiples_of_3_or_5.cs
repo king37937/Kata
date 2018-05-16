7f100da2619d47bbc65d5016c3035dd495c8bbf2
king.ho99 <king.ho@titansoft.com.sg>
2018-05-17 00:57:20 +0800

    refactor

--- KataMultiples_of_3_or_5/KataMultiples_of_3_or_5/Multiples_of_3_or_5.cs
+++ KataMultiples_of_3_or_5/KataMultiples_of_3_or_5/Multiples_of_3_or_5.cs
@@ -10,16 +10,16 @@ namespace KataMultiples_of_3_or_5
     {
         public int Sum(int n)
         {
-            var list = GetList(n);
+            var list = GetList(n, item => item % 3 == 0 || item % 5 == 0);
             return list.Sum();
         }
 
-        private IEnumerable<int> GetList(int n)
+        private IEnumerable<int> GetList(int n, Func<int, bool> predicate)
         {
             var list = Enumerable.Range(1, n - 1);
             foreach (var item in list)
             {
-                if (item % 3 == 0 || item % 5 == 0)
+                if (predicate(item))
                 {
                     yield return item;
                 }
