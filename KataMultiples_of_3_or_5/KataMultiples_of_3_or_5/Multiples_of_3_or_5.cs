8872036a1680216b4b240f080e465415ddcc48aa
king.ho99 <king.ho@titansoft.com.sg>
2018-05-17 00:51:44 +0800

    initial project and add first test case

---
+++ KataMultiples_of_3_or_5/KataMultiples_of_3_or_5/Multiples_of_3_or_5.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataMultiples_of_3_or_5
+{
+    public class Multiples_of_3_or_5
+    {
+        public int Sum(int n)
+        {
+            var list = GetList(n);
+            return list.Sum();
+        }
+
+        private IEnumerable<int> GetList(int n)
+        {
+            var list = Enumerable.Range(1, n - 1);
+            foreach (var item in list)
+            {
+                if (item % 3 == 0 || item % 5 == 0)
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+}
---
+++ KataMultiples_of_3_or_5/KataMultiples_of_3_or_5Tests/Multiples_of_3_or_5Tests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KataMultiples_of_3_or_5;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataMultiples_of_3_or_5.Tests
+{
+    [TestClass()]
+    public class Multiples_of_3_or_5Tests
+    {
+        [TestMethod()]
+        public void SampleCase()
+        {
+            var target = new Multiples_of_3_or_5();
+            var number = 10;
+            var expected = 23;
+
+            var actual = target.Sum(number);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
