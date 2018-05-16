fb4d2816e1660a2f98c76d9bdf82047dadf2d7cb
king.ho99 <king.ho@titansoft.com.sg>
2018-05-16 23:50:12 +0800

    initial project & first test case

---
+++ KataFindMissingTerm/KataFindMissingTerm/FindMissingTerm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataFindMissingTerm
+{
+    public class FindMissingTerm
+    {
+        public int FindMissing(List<int> list)
+        {
+            var difference = FindConstantDifference(list);
+            var expectValue = list[0];
+            foreach (var term in list)
+            {
+                if (term != expectValue)
+                {
+                    return expectValue;
+                }
+
+                expectValue = term + difference;
+            }
+
+            return expectValue;
+        }
+
+        private int FindConstantDifference(List<int> list)
+        {
+            return Math.Min(list[1] - list[0], list[list.Count - 1] - list[list.Count - 2]);
+        }
+    }
+}
---
+++ KataFindMissingTerm/KataFindMissingTermTests/FindMissingTermTests.cs
@@ -0,0 +1,27 @@
+﻿using KataFindMissingTerm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace KataFindMissingTerm.Tests
+{
+    [TestFixture]
+    public class FindMissingTermTests
+    {
+        [Test()]
+        public void FindMissingTest_input_1_3_5_9_7_should_return_7()
+        {
+            var list = new List<int>{1,3,5,9};
+            var target = new FindMissingTerm();
+            var expected = 7;
+
+            var actual = target.FindMissing(list);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
