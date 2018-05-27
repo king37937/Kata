ecc159de93568150acab9859f1c7fe4ed3fb6cc0
king.ho99 <king.ho@titansoft.com.sg>
2018-05-27 22:46:15 +0800

    initial project and first test case

---
+++ KataHighestRankNumber/KataHighestRankNumber/Kata.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataHighestRankNumber
+{
+    public class Kata
+    {
+        public int HighestRank(int[] arr)
+        {
+            var list = new List<int>(arr);
+            var result = list.GroupBy(x => x).OrderByDescending(x => x.Count()).ThenByDescending(x => x.Key);
+
+            return result.First().Key;
+        }
+    }
+}
---
+++ KataHighestRankNumber/KataHighestRankNumberTests/KataTests.cs
@@ -0,0 +1,23 @@
+﻿using KataHighestRankNumber;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+
+namespace KataHighestRankNumber.Tests
+{
+    [TestFixture()]
+    public class KataTests
+    {
+        [TestCase(new[] { 1 }, ExpectedResult = 1)]
+        [TestCase(new[] { 10 }, ExpectedResult = 10)]
+        public int Test_SingleElement(int[] array)
+        {
+            var target = new Kata();
+            return target.HighestRank(array);
+        }
+    }
+}
