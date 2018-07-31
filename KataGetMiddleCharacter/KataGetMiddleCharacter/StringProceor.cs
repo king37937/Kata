e0e2bbf63b632cadf1227d6164871d5f36edd0d4
king.ho99 <king.ho@titansoft.com.sg>
2018-07-31 12:50:23 +0800

    initial project & first test case

---
+++ KataGetMiddleCharacter/KataGetMiddleCharacter/StringProceor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace KataGetMiddleCharacter
+{
+    public class StringProceor
+    {
+        public string GetMiddle(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return string.Empty;
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ KataGetMiddleCharacter/KataGetMiddleCharacterTests/GetMiddleCharacterTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+using KataGetMiddleCharacter;
+
+namespace KataGetMiddleCharacterTests
+{
+    [TestFixture]
+    public class GetMiddleCharacterTests
+    {
+        [Test]
+        public void input_empty_should_return_empty()
+        {
+            var target = new StringProceor();
+            var str = "";
+            var expected = "";
+            var actual = target.GetMiddle(str);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
